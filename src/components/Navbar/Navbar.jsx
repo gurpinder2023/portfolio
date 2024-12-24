77c6e1d8e1ba49e624774e9f43802910777d80eb
gurpinder2023 <gurpinderk714@gmail.com>
2024-12-23 23:47:19 -0800

    styled the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { getImageUrl } from '../../utils';
+import Styles from './Navbar.module.css';
 
 export const Navbar = () => {
-    return <div>Navbar</div>;   
+    return <nav className = {Styles.navbar}>
+        <a className= {Styles.title} href="/">Portfolio</a>
+        <div className= {Styles.menu}>
+            <img className ={Styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button"/>
+            <ul className= {Styles.menuItems}>  
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>;   
 };

e204e3545ff7c119d5e835145a664f0654acb5db
gurpinder2023 <gurpinderk714@gmail.com>
2024-12-31 22:08:36 -0800

    basic navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,15 +4,27 @@ import { getImageUrl } from '../../utils';
 import Styles from './Navbar.module.css';
 
 export const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
     return <nav className = {Styles.navbar}>
         <a className= {Styles.title} href="/">Portfolio</a>
         <div className= {Styles.menu}>
-            <img className ={Styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button"/>
-            <ul className= {Styles.menuItems}>  
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
+            <img className ={Styles.menuBtn} src={
+                menuOpen 
+                ? getImageUrl("nav/closeIcon.png")
+                : getImageUrl("nav/menuIcon.png")
+            } 
+            alt="menu-button"
+            onClick={() => setMenuOpen(!menuOpen)}
+            />
+            <ul 
+            className= {`${Styles.menuItems} ${menuOpen && Styles.menuOpen}`}
+            onclick = {() => setMenuOpen(false)}
+            >  
+                <li><a href="#about" onClick={() => setMenuOpen(false)}>About</a></li>
+                <li><a href="#experience" onClick={() => setMenuOpen(false)}>Experience</a></li>
+                <li><a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a></li>
+                <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a></li>
             </ul>
         </div>
     </nav>;   
